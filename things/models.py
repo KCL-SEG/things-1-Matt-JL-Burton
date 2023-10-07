be0cf9234936f28b25590c1e6bbaff77deb285a4
Matthew Burton <matthew.1.burton@kcl.ac.uk>
2023-10-07 20:45:44 +0100

    Add a User model and migrate it

--- things/models.py
+++ things/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    description=models.TextField()
+    name = models.TextField()
+    quantity = models.IntegerField()
 # Create your models here.
