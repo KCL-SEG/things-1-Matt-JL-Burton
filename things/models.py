5d724200c79529be0dec8c9228ec44380f81e991
Matt-JL-Burton <mslburton24@yahoo.com>
2023-10-15 11:46:16 +0100

    add Thing verifcation

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Thing(AbstractUser):
-    name = models.CharField(max_length=30)
-    description=models.TextField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description=models.TextField(max_length=120)
     quantity = models.IntegerField()
 # Create your models here.
