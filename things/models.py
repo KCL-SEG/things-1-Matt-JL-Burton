e2699398cc713028a8e72aed0362c3f257e4c547
Matt-JL-Burton <mslburton24@yahoo.com>
2023-10-15 12:01:03 +0100

    change values on the max_length parameters to be inclusive

--- things/models.py
+++ things/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import AbstractUser
 class Thing(AbstractUser):  
 
     def validate_quantity_range(value):
-        if value < 0 or value > 100:
+        if value <= 0 or value >= 100:
             raise ValidationError('Quantity must be between 0 and 100.')
 
-    name = models.CharField(max_length=30, unique=True, blank=False)
+    name = models.CharField(max_length=31, unique=True, blank=False)
     username = models.CharField(max_length=30, unique=True, blank=False)
-    description=models.CharField(unique=False, blank=True,max_length=120)
+    description=models.CharField(unique=False, blank=True,max_length=121)
     quantity = models.IntegerField(unique=False, blank=False, validators=[validate_quantity_range])
     password = models.CharField(unique=True, blank=False, max_length=30)
 
