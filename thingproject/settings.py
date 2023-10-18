7458e40862581460fd7b508e35dccf8b89cfe42a
Matt-JL-Burton <mslburton24@yahoo.com>
2023-10-18 13:56:56 +0100

    Add correct validators to quantity field in User model

--- thingproject/settings.py
+++ thingproject/settings.py
@@ -126,4 +126,4 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # # Our USER model for authentication
-AUTH_USER_MODEL='things.Thing'
+# AUTH_USER_MODEL='things.Thing'
--- things/models.py
+++ things/models.py
@@ -1,20 +1,23 @@
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.db.models import Model
+from django.core.validators import MinValueValidator
+from django.core.validators import MaxValueValidator
 
-class Thing(AbstractUser):  
+
+class Thing(Model):  
 
     def validate_quantity_range(value):
         if value <= 0 or value >= 100:
             raise ValidationError('Quantity must be between 0 and 100.')
 
     name = models.CharField(max_length=30, unique=True, blank=False)
-    username = models.CharField(max_length=30, unique=True, blank=False)
+    # username = models.CharField(max_length=30, unique=True, blank=False)
     description=models.CharField(unique=False, blank=True,max_length=120, name='description')
-    quantity = models.IntegerField(unique=False, blank=False, validators=[validate_quantity_range],name='quantity')
-    password = models.CharField(unique=True, blank=False, max_length=30)
+    quantity = models.IntegerField(unique=False, blank=False, validators=[MaxValueValidator(100, message=None), MinValueValidator(0,message=None)],name='quantity')
+    # password = models.CharField(unique=True, blank=False, max_length=30)
 
 
-# Create your models here.
+# Create your Model here.
 
 
